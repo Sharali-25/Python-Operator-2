88e1468bf880bd0e085ad7a8c699cd4d168bfca9
Sharali-25 <Sharaliarkansas@gmail.com>
2025-11-29 08:56:44 -0600

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,21 @@
+a=10
+b=12
+c=0
+if a and  b and c:
+    print("all the number has boolean value")
+else:
+    print("at least one number has boolean number as false")
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("either of number is greater than 0")
+else:
+    print("no number is greater than 0")
+a=10
+b=-10
+c=0
+if b>0 or c>0:
+   print("either number is greater than 0")
+else:
+    print("no number is greater than 0")
